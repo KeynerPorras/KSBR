da80ebe4533bb047c3e05165c91a202defd1829e
KeynerPorras <kporrascruz@gmail.com>
2022-10-04 14:18:35 -0600

    configuracion de tema

--- cliente/ksbrAngular/src/app/home/home.module.ts
+++ cliente/ksbrAngular/src/app/home/home.module.ts
@@ -4,7 +4,8 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { InicioComponent } from './inicio/inicio.component';
 import { AcercaDeComponent } from './acerca-de/acerca-de.component';
-
+import {MatButtonModule} from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,9 @@ import { AcercaDeComponent } from './acerca-de/acerca-de.component';
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    MatButtonModule,
+    MatIconModule 
   ]
 })
 export class HomeModule { }
