2fa3da3e3591fa82f95145d397790c4efc6dc6ab
KeynerPorras <kporrascruz@gmail.com>
2022-10-08 16:57:43 -0600

    cambios mesas

--- cliente/ksbrAngular/src/app/mesas/mesas.module.ts
+++ cliente/ksbrAngular/src/app/mesas/mesas.module.ts
@@ -5,6 +5,12 @@ import { MesasRoutingModule } from './mesas-routing.module';
 
 import {MatGridListModule} from '@angular/material/grid-list';
 import { GestionMesasComponent } from './gestion-mesas/gestion-mesas.component';
+import {MatIconModule} from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+
+import { MatButtonModule } from '@angular/material/button';
+import { LayoutModule } from '@angular/cdk/layout';
 
 
 @NgModule({
@@ -14,7 +20,12 @@ import { GestionMesasComponent } from './gestion-mesas/gestion-mesas.component';
   imports: [
     CommonModule,
     MesasRoutingModule,
-    MatGridListModule
+    MatGridListModule,
+    MatIconModule,
+    MatCardModule,
+    MatMenuModule,
+    MatButtonModule,
+    LayoutModule
   ],
   exports: [  
   ]
