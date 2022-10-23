d9ce447e2044b109badc4c87fc32977daa092a51
KeynerPorras <kporrascruz@gmail.com>
2022-10-23 10:46:21 -0600

    mesasApi

--- cliente/ksbrAngular/src/app/core/header/header.component.ts
+++ cliente/ksbrAngular/src/app/core/header/header.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  isAutenticated: boolean = false;
+  isAutenticated: any;
   currentUser: any;
   constructor() { }
 
--- cliente/ksbrAngular/src/app/mesas/gestion-mesas/gestion-mesas.component.ts
+++ cliente/ksbrAngular/src/app/mesas/gestion-mesas/gestion-mesas.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+import { GenericService } from 'src/app/share/generic.service';
 
 @Component({
   selector: 'app-gestion-mesas',
@@ -6,8 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gestion-mesas.component.css']
 })
 export class GestionMesasComponent implements OnInit {
+  datos: any;
+  destroy$: Subject<boolean> = new Subject<boolean>();
 
-  constructor() { }
+  constructor(
+    private gSevice: GenericService
+  ) { this.listaVideojuegos();}
+
+  listaVideojuegos() {
+    this.gSevice
+      .list('mesa/')
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((data: any) => {
+        console.log(data);
+        this.datos = data;
+      });
+  }
+
+
 
   ngOnInit(): void {
   }
